c1337ef532968e73ecf792dc2d49ad25f05e1b61
KimTaeYeon <kuarnty@gmail.com>
2021-09-28 21:10:27 +0900

    브루트포스 관련 문제 풀이

---
+++ BOJ/11170_0의_개수.cpp
@@ -0,0 +1,38 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <iostream>
+//#include <stdio.h>
+//#include <cstring>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	
+//	int n, m;
+//	char c[8];
+//
+//	for (int i = 0; i < t; i++)
+//	{
+//		int count = 0;
+//
+//		cin >> n >> m;
+//
+//		for (int j = n; j <= m; j++)
+//		{
+//			sprintf(c, "%d", j);
+//
+//			for (int k = 0; k < strlen(c); k++)
+//			{
+//				if (c[k] == '0')
+//					count++;
+//			}			
+//		}
+//
+//		cout << count << endl;
+//	}
+//
+//	return 0;
+//}
--- BOJ/18870_좌표_압축.cpp
+++ BOJ/18870_좌표_압축.cpp
@@ -1,23 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	int n;
-	cin >> n;
-	vector<int> v(n);
-
-	for (int i = 0; i < n; i++)
-	{
-		v.
-	}
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(0);
+//
+//	int n;
+//	cin >> n;
+//	vector<int> v(n);
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		v.
+//	}
+//
+//	return 0;
+//}
