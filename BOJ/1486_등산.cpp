e24c5ccf117ecd930afe22b0e1b5621fae068786
KimTae-yeon <kuarnty@gmail.com>
2022-12-17 23:03:25 +0900

    ...

---
+++ BOJ/1486_등산.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#define endl			'\n'
+
+#ifndef INT_MAX
+#define INT_MAX			2147483647
+#define INT_MIN			-2147483648
+#define LONG_LONG_MAX	9223372036854775807
+#define LONG_LONG_MIN	-9223372036854775808
+#endif
+
+#define ABS(A)			(((A) > 0) ? (A) : (-A))
+#define MAX(A,B)		(((A) > (B)) ? (A) : (B))
+#define MIN(A,B)		(((A) < (B)) ? (A) : (B))
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	int N, M, T, D;
+	cin >> N >> M >> T >> D;
+	vector<vector<int>> map(25, vector<int>(25));
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			cin >> map[i][j];
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			map[i][j] -= map[i][j] <= (int)('Z') ? 'A' : 'a' - 26;
+
+
+
+
+	return 0;
+}
